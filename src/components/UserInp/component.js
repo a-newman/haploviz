bcda5369b21d508d45dcc2e0832c06a62168271a
a-newman <apnewman@mit.edu>
2016-10-22 18:15:55 -0400

    Initial commit

---
+++ src/components/UserInp/component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class UserInp extends Component {
+  render() {
+    return (
+      <h1>This will be my user input dropdown!</h1>
+    );
+  }
+}; 
+
+export default UserInp;
