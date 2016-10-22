b79035094281dcf804ababae96392bfa879b75df
a-newman <apnewman@mit.edu>
2016-10-22 19:12:03 -0400

    Initial selection dropdown

--- src/components/UserInp/component.js
+++ src/components/UserInp/component.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
 
+function Options(props) {
+    const defaultItem = [
+      <option disabled key='null' value='default'>
+        {props.defaultMessage}
+      </option>
+    ]; 
+
+    //const optionItems = props.options.map((option) => 
+    const optionItems = defaultItem.concat(["option1", "option2"].map((option) => 
+      <option key={option} value={option}>
+        {option}
+      </option>
+    ));
+
+    return (
+      <select defaultValue={props.value}>
+        {optionItems}
+      </select>
+    ); 
+}
+
 class UserInp extends Component {
+  constructor(props) {
+    //props should contain an option like 'options' that has drpodown options
+    super(props); 
+    this.state = {value: 'default'}; 
+  }
+
   render() {
     return (
-      <h1>This will be my user input dropdown!</h1>
+      <div className="userinp">
+        <h1>This is the user input dropdown!</h1>
+        <Options options='' defaultMessage='Select an annotation' value={this.state.value}/>
+      </div>
     );
   }
 }; 
